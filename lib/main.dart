fe03c3f002a59972b4de940dcb2f2cfad681dc36
Levent Kantaroğlu <leventkantaroglu@dusyeri.com.tr>
2021-12-26 23:31:32 +0300

    remove debugBanner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
